9cd7a5519804505b57071e4e6c3e83663efc604f
DawidBudzynsky <dawidbudzynski18@gmail.com>
2024-05-16 23:05:40 +0200

    added exceptions texts

--- projekt/src/parser/exceptions.py
+++ projekt/src/parser/exceptions.py
@@ -1,9 +1,38 @@
 MISSING_EXPRESSION = "Error: Missing expresion after [%c] operator; [%d:%d]"
 MISSING_STATEMENT = "Error: Missing statement [%s]; [%d:%d]"
-INVALID_SYNTAX = "Error: Invalid syntax, wanted: [%s], got: [%s]; [%d:%d]"
+INVALID_SYNTAX = "Error: Invalid syntax, wanted: [%s] %s; [%d:%d]"
 TYPE_ANNOTATION = "Error: Missing type annotation; [%d:%d]"
 FUNC_REDEFINITION = "Error: Function redefinition, function name: [%s]; [%d:%d]"
 ASPECT_REDEFINITION = "Error: Aspect redefinition, aspect name: [%s]; [%d:%d]"
+VERBOSE = "Error: %s; [%d:%d]"
+
+MISSING_IDENTIFIER_AFTER_COMMA = "Missing identifier after [,]"
+ASSIGN_OR_CALL_MISSING = "Missing [%s] or [%s] after identifier"
+FUN_DEF_PAREN_OPEN_MISSING = (
+    "You should use [(] after identifier when defining a function"
+)
+
+FUN_DEF_PAREN_CLOSE_MISSING = (
+    "Function definition not closed, you should use [)] after parameters"
+)
+
+ASPECT_DEF_PAREN_OPEN_MISSING = "Use [(] after identifier when defining an aspect"
+ASPECT_MISSING_NAME = "Missing aspect name"
+ASPECT_DEF_NOT_CLOSED = "Aspect definition not closed"
+ASPECT_BLOCK_MISSING = "No block defined for aspect"
+ASPECT_BLOCK_NOT_CLOSED = "Aspect block not closed"
+FUN_CALL_NOT_CLOSED = "Function call not closed"
+ETX_MISSING = "No End of Text symbol were found"
+PARAMETER_IDENTIFIER = "Parameter must be an identifier"
+PARAMETER_COLON_MISSING = "Use [:] after identifier when defining a parameter"
+BLOCK_NOT_CLOSED = "Block is not closed"
+VAR_DECLARATION_IDENTIFIER = (
+    "You should name your variable, use identifier after [type]"
+)
+VAR_DECLARATION_COLON = "Use [=] to assign value to variable"
+EXPREESION_NOT_CLOSED = "Expression not closed"
+FOR_EACH_IDENTIFIER = "Define what identifier to iterate over"
+FOR_EACH_IN_MISSING = "Use [in] after identifier in for loop"
 
 
 class FunctionRedefinition(Exception):
